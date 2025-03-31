547a2750d3a2971f01f35fd2fe8b83658746aa35
LindaCristalParra <linda.parra@est.fi.uncoma.edu.ar>
2025-03-31 19:16:58 -0300

    -Validación año bisisesto

--- TP1/Fecha/Fecha.php
+++ TP1/Fecha/Fecha.php
@@ -71,7 +71,11 @@ class Fecha
 
     public function bisiesto($anio)
     {
-
+        if(($anio % 4 == 0 && $anio % 100 != 0) || ($anio % 400 == 0)){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function incremento($dias, $fecha)
