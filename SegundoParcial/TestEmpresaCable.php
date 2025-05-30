3e9b86fe6fd729675f33b6bd98a7ed082d4d3dbb
LindaCristalparra <linda.parra@est.fi.uncoma.edu.ar>
2025-05-30 18:14:11 -0300

    - test

--- SegundoParcial/TestEmpresaCable.php
+++ SegundoParcial/TestEmpresaCable.php
@@ -7,3 +7,10 @@ $canal3 = new Canal("Canal 3", "Descripción del Canal 3");
 $plan1 = new Plan("Plan Básico", 100, 10, [$canal1, $canal2]);
 $plan2 = new Plan("Plan Premium", 200, 20, [$canal1, $canal2, $canal3]);
 $cliente = new Cliente("Juan Pérez", "12345678", "Calle Falsa 123");
+$cliente2 = new Cliente("Ana Gómez", "87654321", "Avenida Siempre Viva 456");
+
+$contrato1 = new Contrato($cliente, $plan1, "2023-01-01", "2024-01-01");
+$contrato2 = new Contrato($cliente, $plan2, "2023-02-01", "2024-02-01");
+$contrato3 = new Contrato($cliente, $plan1, "2023-03-01", "2024-03-01");
+$empresa->incorporarContrato($contrato1, $$cliente, new DateTime("2023-01-01"), new DateTime("2024-01-01"), true) ;
+$empresa2->incorporarContrato($contrato2, $cliente2, new DateTime("2023-02-01"), new DateTime("2024-02-01"), false);
