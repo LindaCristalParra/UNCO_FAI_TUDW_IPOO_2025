726213c68c11353ba42e50a8b65e647c77f65dcb
LindaCristalparra <linda.parra@est.fi.uncoma.edu.ar>
2025-05-30 15:34:22 -0300

    - Format document

--- SegundoParcial/Canal.php
+++ SegundoParcial/Canal.php
@@ -1,42 +1,51 @@
 <?php
-class Canal{
+class Canal
+{
     private string $tipoCanal;
     private float $importe;
     private bool $esHD;
 
-    public function __construct(string $tipoCanal, float $importe, bool $esHD) {
+    public function __construct(string $tipoCanal, float $importe, bool $esHD)
+    {
         $this->tipoCanal = $tipoCanal;
         $this->importe = $importe;
         $this->esHD = $esHD;
     }
 
-    public function getTipoCanal(): string {
+    public function getTipoCanal(): string
+    {
         return $this->tipoCanal;
     }
 
-    public function getImporte(): float {
+    public function getImporte(): float
+    {
         return $this->importe;
     }
 
-    public function getEsHD(): bool {
+    public function getEsHD(): bool
+    {
         return $this->esHD;
     }
 
-    public function setTipoCanal(string $tipoCanal): void {
+    public function setTipoCanal(string $tipoCanal): void
+    {
         $this->tipoCanal = $tipoCanal;
     }
 
-    public function setImporte(float $importe): void {
+    public function setImporte(float $importe): void
+    {
         $this->importe = $importe;
     }
 
-    public function setEsHD(bool $esHD): void {
+    public function setEsHD(bool $esHD): void
+    {
         $this->esHD = $esHD;
     }
-    public function __toString(): string {
+    public function __toString(): string
+    {
         return "Tipo de Canal: " . $this->getTipoCanal() . "\n" .
-               "Importe: " . $this->getImporte() . "\n" .
-               "¿Es HD?: " . ($this->getEsHD() ? 'Sí' : 'No') . "\n";
+            "Importe: " . $this->getImporte() . "\n" .
+            "¿Es HD?: " . ($this->getEsHD() ? 'Sí' : 'No') . "\n";
     }
 }
 
--- SegundoParcial/Plan.php
+++ SegundoParcial/Plan.php
@@ -1,65 +1,76 @@
-<?php 
-class Plan {
+<?php
+class Plan
+{
 
     private int $codigo;
     private array $colCanales;
     private float $importe;
     private bool $mgIncluido;
 
-    public function __construct(int $codigo, array $colCanales, float $importe) {
+    public function __construct(int $codigo, array $colCanales, float $importe)
+    {
         $this->codigo = $codigo;
         $this->colCanales = $colCanales;
         $this->importe = $importe;
         $this->mgIncluido = 100; // Por defecto, el MG está incluido
     }
 
-    public function getCodigo(): int {
+    public function getCodigo(): int
+    {
         return $this->codigo;
     }
 
-    public function getColCanales(): array {
+    public function getColCanales(): array
+    {
         return $this->colCanales;
-    }   
+    }
 
-    public function getImporte(): bool {
+    public function getImporte(): bool
+    {
         return $this->importe;
     }
 
-    public function getMgIncluido(): bool {
+    public function getMgIncluido(): bool
+    {
         return $this->mgIncluido;
     }
 
-    public function setCodigo(int $codigo): void {
+    public function setCodigo(int $codigo): void
+    {
         $this->codigo = $codigo;
     }
 
-    public function setColCanales(array $colCanales): void {
+    public function setColCanales(array $colCanales): void
+    {
         $this->colCanales = $colCanales;
     }
 
-    public function setImporte(bool $importe): void {
+    public function setImporte(bool $importe): void
+    {
         $this->importe = $importe;
     }
 
-    public function setMgIncluido(bool $mgIncluido): void {
+    public function setMgIncluido(bool $mgIncluido): void
+    {
         $this->mgIncluido = $mgIncluido;
     }
 
-    public function __toString(): string {
+    public function __toString(): string
+    {
         $output = "═══════════════════════════════════\n";
         $output .= "          PLAN         \n";
         $output .= "═══════════════════════════════════\n";
-        $output .= "Código:".$this->getCodigo()."\n";
-        $output .= "Importe: ".$this->getImporte()."\n";
+        $output .= "Código:" . $this->getCodigo() . "\n";
+        $output .= "Importe: " . $this->getImporte() . "\n";
         $output .= "Canales:\n";
-        $output .= "-----------------------------------\n"; 
+        $output .= "-----------------------------------\n";
         $output .= $this->convertirAstring($this->getColCanales()) . "\n";
         $output .= "¿Incluye MG?: " . ($this->getMgIncluido() ? 'Sí' : 'No') . "\n";
 
         return $output;
     }
 
-        private function convertirAstring(array $miArray): string
+    private function convertirAstring(array $miArray): string
     {
         $output = "";
         if (empty($miArray)) {
