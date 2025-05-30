3057ef26f2ed7cd5bfd80ebc0b7e8552f4789877
LindaCristalparra <linda.parra@est.fi.uncoma.edu.ar>
2025-05-30 15:24:28 -0300

    - Incorporación de clase Plan del segundo parcial-Contrato de planes de una empresa de cable

---
+++ SegundoParcial/Plan.php
@@ -0,0 +1,75 @@
+<?php 
+class Plan {
+
+    private int $codigo;
+    private array $colCanales;
+    private float $importe;
+    private bool $mgIncluido;
+
+    public function __construct(int $codigo, array $colCanales, float $importe, bool $mgIncluido) {
+        $this->codigo = $codigo;
+        $this->colCanales = $colCanales;
+        $this->importe = $importe;
+        $this->mgIncluido = $mgIncluido;
+    }
+
+    public function getCodigo(): int {
+        return $this->codigo;
+    }
+
+    public function getColCanales(): array {
+        return $this->colCanales;
+    }   
+
+    public function getImporte(): bool {
+        return $this->importe;
+    }
+
+    public function getMgIncluido(): bool {
+        return $this->mgIncluido;
+    }
+
+    public function setCodigo(int $codigo): void {
+        $this->codigo = $codigo;
+    }
+
+    public function setColCanales(array $colCanales): void {
+        $this->colCanales = $colCanales;
+    }
+
+    public function setImporte(bool $importe): void {
+        $this->importe = $importe;
+    }
+
+    public function setMgIncluido(bool $mgIncluido): void {
+        $this->mgIncluido = $mgIncluido;
+    }
+
+    public function __toString(): string {
+        $output = "═══════════════════════════════════\n";
+        $output .= "          PLAN         \n";
+        $output .= "═══════════════════════════════════\n";
+        $output .= "Código:".$this->getCodigo()."\n";
+        $output .= "Importe: ".$this->getImporte()."\n";
+        $output .= "Canales:\n";
+        $output .= "-----------------------------------\n"; 
+        $output .= $this->convertirAstring($this->getColCanales()) . "\n";
+        $output .= "¿Incluye MG?: " . ($this->getUsIncluido() ? 'Sí' : 'No') . "\n";
+
+        return $output;
+    }
+
+        private function convertirAstring(array $miArray): string
+    {
+        $output = "";
+        if (empty($miArray)) {
+            $output .= "No hay elementos.\n";
+        } else {
+            foreach ($miArray as $i => $objeto) {
+                $output .= "#" . ($i + 1) . ":\n";
+                $output .= $objeto . "\n";
+            }
+        }
+        return $output;
+    }
+}
